8e7c7a4ef2b4001dea70ca8d9552a91cef329fc2
Elias <elias@emawa.io>
2023-01-18 18:09:50 -0700

    remove package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,23 @@
-import type { AppProps } from 'next/app';
-import React from 'react';
+import type { AppProps } from "next/app";
+import React from "react";
 
-import { StoreProvider } from '../lib/store/StoreProvider';
-import { createStore, useHydrate } from '../lib/store/store';
+import { StoreProvider } from "../lib/store/StoreProvider";
+import { createStore, useHydrate } from "../lib/store/store";
 
-import 'stagcss';
-import 'stagcss/dist/stag-ext.min.css';
+// import 'stagcss';
+// import 'stagcss/dist/stag-ext.min.css';
 
-import '../css/styles.scss';
+import "../css/styles.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	const state = pageProps.state ? pageProps.state : createStore(pageProps.state);
-	const store = useHydrate(state);
-	
-	return (
-		<StoreProvider store={store}>
-			<Component {...pageProps} />
-		</StoreProvider>
-	);
+  const state = pageProps.state ? pageProps.state : createStore(pageProps.state);
+  const store = useHydrate(state);
+
+  return (
+    <StoreProvider store={store}>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 };
 
 export default MyApp;
